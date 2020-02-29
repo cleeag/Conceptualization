4bb9eb1190317e4fb7e5e1a8761a655b6ed1169c
cleeag <jack0506000@gmail.com>
2020-02-29 18:07:21 +0800

    result done

--- model.py
+++ model.py
@@ -6,12 +6,13 @@ from scipy.sparse import *
 from tqdm import tqdm
 import numpy as np
 import scipy
+import csv
 
 import networkx as nx
 from sklearn.metrics.pairwise import cosine_similarity
 import torch.nn as nn
 
-from utils import read_data, co_occurence_lookup
+from utils import read_data, co_occurence_lookup, inspect_result
 
 
 class GDConcept:
@@ -20,44 +21,6 @@ class GDConcept:
         self.tolerence = tolerence
         self.alpha = alpha
         self.concept_num = concept_num
-
-    # get sum of feature function
-    def _get_sum_of_feature(self, seq, cliques):
-        print('getting sum of features')
-        sum_f = csc_matrix((self.concept_num, 1))
-        for clique in cliques:
-            if len(clique) == 1: continue
-            E_k = seq[clique[1], :]
-            E_k[E_k > 0] = 1
-            tmp0 = np.prod(E_k.toarray(), axis=0)
-            tmp1 = csc_matrix(tmp0)
-            tmp2 = E_k.sum(axis=0)
-            sum_f += tmp1.multiply(tmp2).reshape(-1, 1)
-            # sum_f += np.multiply(tmp1, tmp2).reshape(-1, 1)
-            # sum_f += np.multiply(np.prod(E_k, axis=0), np.sum(E_k, axis=0)).reshape(-1, 1)
-
-        return sum_f
-
-    # get C_opt
-    def _inference_C(self, sum_f):
-        print('inferencing C')
-        c_opt = (self.alpha - 1 + sum_f) / sum(self.alpha - 1 + sum_f)
-
-        return c_opt
-
-    # get cliques in sequence
-    def _clique_detection(self, seq):
-        print('detecting cliques...')
-        G = nx.Graph()
-        G.add_nodes_from(range(seq.shape[0]))
-        for comb in itertools.combinations(range(seq.shape[0]), 2):
-            cos = cosine_similarity(seq[comb[0]].reshape(1, -1), seq[comb[1]].reshape(1, -1))
-            if  cos > self.tolerence:
-                G.add_edge(comb, 1)
-        print('nx finding cliques')
-        cliques = nx.find_cliques(G)
-
-        return cliques
 
     def _alpha_estimation(self, sum_f_list):
         print('estimating alpha')
@@ -89,7 +52,50 @@ class GDConcept:
 
         return alpha
 
-    def inference(self, input_data):
+    # get sum of feature function
+    def _get_sum_of_feature(self, seq, cliques):
+        print('getting sum of features')
+        sum_f = csc_matrix((self.concept_num, 1))
+        for clique in cliques:
+            if len(clique) == 1: continue
+            print(clique)
+            E_k = seq[clique[1], :]
+            E_k[E_k > 0] = 1
+            tmp0 = np.prod(E_k.toarray(), axis=0)
+            tmp1 = csc_matrix(tmp0)
+            tmp2 = seq[clique[1], :].sum(axis=0)
+            sum_f += tmp1.multiply(tmp2).reshape(-1, 1)
+
+        for i in range(seq.shape[0]):
+            E_k = seq[i, :]
+            E_k[E_k > 0] = 1
+            sum_f += E_k.multiply(seq[i, :]).reshape(-1, 1)
+
+        return sum_f
+
+    # get C_opt
+    def _inference_C(self, sum_f):
+        sum_f = sum_f.toarray()
+        tmp = self.alpha - 1
+        c_opt = (tmp + sum_f) / sum(tmp + sum_f)
+
+        return c_opt
+
+    # get cliques in sequence
+    def _clique_detection(self, seq):
+        print('detecting cliques...')
+        G = nx.Graph()
+        G.add_nodes_from(range(seq.shape[0]))
+        for comb in itertools.combinations(range(seq.shape[0]), 2):
+            cos = cosine_similarity(seq[comb[0]].reshape(1, -1), seq[comb[1]].reshape(1, -1))
+            if cos > self.tolerence:
+                G.add_edge(comb, 1)
+        print('nx finding cliques')
+        cliques = nx.find_cliques(G)
+
+        return cliques
+
+    def inference(self, input_data, result_path):
         clique_list = []
         sum_f_list = []
         for idx, seq in enumerate(tqdm(input_data)):
@@ -102,17 +108,17 @@ class GDConcept:
         if self.alpha is None:
             self.alpha = self._alpha_estimation(sum_f_list)
 
+        print('inferencing C')
         C = []
         for idx, seq in enumerate(tqdm(input_data)):
             c_opt = self._inference_C(sum_f_list[idx])
             C.append(c_opt)
-
-        print(C[0])
+        pkl.dump(C, file=open(result_path, 'wb'))
         return C
 
 
 def run():
-    tolerence = 0
+    tolerence = 0.1
     concept_num = 2359855
     instance_num = 6215859
     # input_data = [np.random.randint(20, size=(np.random.randint(3, 15), concept_num)) for _ in range(20)]
@@ -120,18 +126,23 @@ def run():
     # alpha = csc_matrix((concept_num, 1))
     # alpha = None
     test_data = '/home/data/cleeag/conceptualization/test_data.txt'
-    co_path = '/home/data/cleeag/conceptualization/short-text-conceptualization/conceptInstanceFile'
+    raw_file_dir_path = '/home/data/cleeag/conceptualization/short-text-conceptualization'
     co_matrix_path = '/home/data/cleeag/conceptualization/co_matrix.pkl'
-    inst2idx_path = '/home/data/cleeag/conceptualization/short-text-conceptualization/instanceFile'
     inst2idx_dict_path = '/home/data/cleeag/conceptualization/inst2idx_dict.pkl'
+    idx2concept_dict_path = '/home/data/cleeag/conceptualization/idx2concept_dict.pkl'
+    C_result_path = '/home/data/cleeag/conceptualization/C.pkl'
+    result_path = '/home/data/cleeag/conceptualization/result.txt'
 
     raw_data = read_data(test_data)
-    input_data = co_occurence_lookup(raw_data, concept_num, instance_num,
-                                     co_path=co_path, co_matrix_path=co_matrix_path,
-                                     inst2idx_path=inst2idx_path, inst2idx_dict_path=inst2idx_dict_path)
+    input_data, idx2concept_dict = co_occurence_lookup(raw_data, concept_num, instance_num,
+                                                       raw_file_dir_path=raw_file_dir_path,
+                                                       co_matrix_path=co_matrix_path,
+                                                       inst2idx_dict_path=inst2idx_dict_path,
+                                                       idx2concept_dict_path=idx2concept_dict_path)
 
-    model = GDConcept(tolerence, concept_num, alpha=alpha)
-    model.inference(input_data)
+    # model = GDConcept(tolerence, concept_num, alpha=alpha)
+    # C = model.inference(input_data, C_result_path)
+    inspect_result(raw_data, idx2concept_dict, C_result_path, result_path)
 
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -4,7 +4,9 @@ import itertools
 import pickle as pkl
 from scipy.sparse import *
 from tqdm import tqdm
+import sys
 
+import inflect
 
 def read_data(path):
     data = []
@@ -13,10 +15,12 @@ def read_data(path):
             data.append(line.split())
     return data
 
-def co_occurence_lookup(data, concept_num, instance_num, co_path='', co_matrix_path='', inst2idx_path='', inst2idx_dict_path=''):
+
+def co_occurence_lookup(data, concept_num, instance_num, raw_file_dir_path='',
+                        co_matrix_path='', inst2idx_dict_path='', idx2concept_dict_path=''):
     if not os.path.exists(co_matrix_path):
         co_matrix = lil_matrix((concept_num, instance_num))
-        with open(co_path, 'r') as r:
+        with open(join(raw_file_dir_path, 'conceptInstanceFile'), 'r') as r:
             for i, line in enumerate(tqdm(r)):
                 concept, instance, co_value = [int(x) for x in line.split()]
                 co_matrix[concept, instance] = co_value
@@ -28,28 +32,80 @@ def co_occurence_lookup(data, concept_num, instance_num, co_path='', co_matrix_p
 
     if not os.path.exists(inst2idx_dict_path):
         inst2idx_dict = dict()
-        with open(inst2idx_path, 'r') as r:
+        with open(join(raw_file_dir_path, 'instanceFile'), 'r') as r:
             for i, line in enumerate(tqdm(r)):
-                instance, idx= line.split('\t')
+                instance, idx = line.split('\t')
                 inst2idx_dict[instance] = int(idx)
         pkl.dump(inst2idx_dict, file=open(inst2idx_dict_path, 'wb'), )
-
     else:
         print('loading inst2idx_dict...')
         inst2idx_dict = pkl.load(file=open(inst2idx_dict_path, 'rb'))
 
+    if not os.path.exists(idx2concept_dict_path):
+        idx2concept_dict = dict()
+        with open(join(raw_file_dir_path, 'conceptFile'), 'r') as r:
+            for i, line in enumerate(tqdm(r)):
+                concept, idx = line.split('\t')
+                idx2concept_dict[int(idx)] = concept
+        pkl.dump(idx2concept_dict, file=open(idx2concept_dict_path, 'wb'), )
+    else:
+        print('loading idx2concept_dict...')
+        idx2concept_dict = pkl.load(file=open(idx2concept_dict_path, 'rb'))
+
     vec_data = []
+    p = inflect.engine()
     for line in data:
-        tmp = csc_matrix((len(line), concept_num))
+        tmp=[]
         for i, word in enumerate(line):
+            word = ''.join(e for e in word if e.isalnum())
+            singular_word = p.singular_noun(word)
+            word = singular_word if singular_word else word
             idx = inst2idx_dict.get(word)
             if not idx:
                 continue
-            tmp[i] = co_matrix[:, idx].T
+            tmp.append([co_matrix[:, idx].T])
+        tmp = bmat(tmp, format='csc')
         vec_data.append(tmp)
 
-    return vec_data
+    return vec_data, idx2concept_dict
+
+def check_dict(inst2idx_dict_path):
+    idx2concept_dict = pkl.load(file=open(inst2idx_dict_path, 'rb'))
+    while True:
+        key = input('please input key\n')
+        print(idx2concept_dict.get(key, 'not found'))
+
+
+def inspect_result(raw_data, idx2concept_dict, result_path, output_path):
+    C = pkl.load(file=open(result_path, 'rb'))
+    with open(output_path, 'w') as w:
+        for idx, c_opt in enumerate(C):
+            top_3 = c_opt.flatten().argsort()[-5:][::-1]
+            print(idx)
+            print(raw_data[idx])
+            w.write(f'({str(idx)})   {raw_data[idx]}\n')
+            for x in top_3:
+                w.write('\t'.join([str(x), str(c_opt[x]), idx2concept_dict[x]]) + '\n')
+                print(x, c_opt[x], idx2concept_dict[x])
+            print()
+            w.write('\n')
 
 
 if __name__ == '__main__':
-    pass
+    test_data = '/home/data/cleeag/conceptualization/test_data.txt'
+    raw_file_dir_path = '/home/data/cleeag/conceptualization/short-text-conceptualization'
+    co_matrix_path = '/home/data/cleeag/conceptualization/co_matrix.pkl'
+    inst2idx_dict_path = '/home/data/cleeag/conceptualization/inst2idx_dict.pkl'
+    idx2concept_dict_path = '/home/data/cleeag/conceptualization/idx2concept_dict.pkl'
+    concept_num = 2359855
+    instance_num = 6215859
+
+    # check_dict(inst2idx_dict_path)
+    # sys.exit()
+
+    raw_data = read_data(test_data)
+    input_data, idx2concept_dict = co_occurence_lookup(raw_data, concept_num, instance_num,
+                                                       raw_file_dir_path=raw_file_dir_path,
+                                                       co_matrix_path=co_matrix_path,
+                                                       inst2idx_dict_path=inst2idx_dict_path,
+                                                       idx2concept_dict_path=idx2concept_dict_path)
